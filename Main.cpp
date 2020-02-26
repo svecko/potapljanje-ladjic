a199496a424acf4de6036363bf21b0615a4d90a7
Miha Svecak <miha.svecak@gmail.com>
2020-02-26 21:01:28 +0100

    Added Player Field Logger

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@
 
 void VpisTabela(char tabela[][8], int n);
 void VpisLadjic(char tabela[][8], int n);
-void IzpisTabela(char tabela[][8], int n);
+void IzpisTabela(char tabela[][8], int n, const char* imeIgralnoPolje);
 
 int main()
 {
@@ -41,7 +41,7 @@ void VpisTabela(char tabela[][8], int n)
 
 void VpisLadjic(char tabela[][8], int n)
 {
-	IzpisTabela(tabela, n);
+	IzpisTabela(tabela, n, "Miha");
 
 	char pozicija[3];
 	std::cout << "Vnesi pozicijo letalonosilke ([stolpec][vrstica] ali [vrstica][stolpec])" << std::endl;
@@ -166,11 +166,510 @@ void VpisLadjic(char tabela[][8], int n)
 		}
 	}
 
-	IzpisTabela(tabela, n);
+	IzpisTabela(tabela, n, "Miha");
+
+	std::cout << "Vnesi pozicijo bojne ladje ([stolpec][vrstica] ali [vrstica][stolpec])" << std::endl;
+	gets_s(pozicija);
+
+	if (pozicija[0] >= 'a' && pozicija[0] <= 'g')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case 'a':
+			i = 1;
+			break;
+		case 'b':
+			i = 2;
+			break;
+		case 'c':
+			i = 3;
+			break;
+		case 'd':
+			i = 4;
+			break;
+		case 'e':
+			i = 5;
+			break;
+		case 'f':
+			i = 6;
+			break;
+		case 'g':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case '1':
+			j = 1;
+			break;
+		case '2':
+			j = 2;
+			break;
+		case '3':
+			j = 3;
+			break;
+		case '4':
+			j = 4;
+			break;
+		case '5':
+			j = 5;
+			break;
+		case '6':
+			j = 6;
+			break;
+		case '7':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 4; k++)
+		{
+			tabela[j][i] = 'B';
+			j++;
+		}
+	}
+	else if (pozicija[0] >= '1' && pozicija[0] <= '7')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case '1':
+			i = 1;
+			break;
+		case '2':
+			i = 2;
+			break;
+		case '3':
+			i = 3;
+			break;
+		case '4':
+			i = 4;
+			break;
+		case '5':
+			i = 5;
+			break;
+		case '6':
+			i = 6;
+			break;
+		case '7':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case 'a':
+			j = 1;
+			break;
+		case 'b':
+			j = 2;
+			break;
+		case 'c':
+			j = 3;
+			break;
+		case 'd':
+			j = 4;
+			break;
+		case 'e':
+			j = 5;
+			break;
+		case 'f':
+			j = 6;
+			break;
+		case 'g':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 4; k++)
+		{
+			tabela[i][j] = 'B';
+			j++;
+		}
+	}
+
+	IzpisTabela(tabela, n, "Miha");
+
+	std::cout << "Vnesi pozicijo podmornice ([stolpec][vrstica] ali [vrstica][stolpec])" << std::endl;
+	gets_s(pozicija);
+
+	if (pozicija[0] >= 'a' && pozicija[0] <= 'g')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case 'a':
+			i = 1;
+			break;
+		case 'b':
+			i = 2;
+			break;
+		case 'c':
+			i = 3;
+			break;
+		case 'd':
+			i = 4;
+			break;
+		case 'e':
+			i = 5;
+			break;
+		case 'f':
+			i = 6;
+			break;
+		case 'g':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case '1':
+			j = 1;
+			break;
+		case '2':
+			j = 2;
+			break;
+		case '3':
+			j = 3;
+			break;
+		case '4':
+			j = 4;
+			break;
+		case '5':
+			j = 5;
+			break;
+		case '6':
+			j = 6;
+			break;
+		case '7':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 3; k++)
+		{
+			tabela[j][i] = 'P';
+			j++;
+		}
+	}
+	else if (pozicija[0] >= '1' && pozicija[0] <= '7')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case '1':
+			i = 1;
+			break;
+		case '2':
+			i = 2;
+			break;
+		case '3':
+			i = 3;
+			break;
+		case '4':
+			i = 4;
+			break;
+		case '5':
+			i = 5;
+			break;
+		case '6':
+			i = 6;
+			break;
+		case '7':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case 'a':
+			j = 1;
+			break;
+		case 'b':
+			j = 2;
+			break;
+		case 'c':
+			j = 3;
+			break;
+		case 'd':
+			j = 4;
+			break;
+		case 'e':
+			j = 5;
+			break;
+		case 'f':
+			j = 6;
+			break;
+		case 'g':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 3; k++)
+		{
+			tabela[i][j] = 'P';
+			j++;
+		}
+	}
+
+	IzpisTabela(tabela, n, "Miha");
+
+	std::cout << "Vnesi pozicijo rusilca ([stolpec][vrstica] ali [vrstica][stolpec])" << std::endl;
+	gets_s(pozicija);
+
+	if (pozicija[0] >= 'a' && pozicija[0] <= 'g')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case 'a':
+			i = 1;
+			break;
+		case 'b':
+			i = 2;
+			break;
+		case 'c':
+			i = 3;
+			break;
+		case 'd':
+			i = 4;
+			break;
+		case 'e':
+			i = 5;
+			break;
+		case 'f':
+			i = 6;
+			break;
+		case 'g':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case '1':
+			j = 1;
+			break;
+		case '2':
+			j = 2;
+			break;
+		case '3':
+			j = 3;
+			break;
+		case '4':
+			j = 4;
+			break;
+		case '5':
+			j = 5;
+			break;
+		case '6':
+			j = 6;
+			break;
+		case '7':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 2; k++)
+		{
+			tabela[j][i] = 'R';
+			j++;
+		}
+	}
+	else if (pozicija[0] >= '1' && pozicija[0] <= '7')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case '1':
+			i = 1;
+			break;
+		case '2':
+			i = 2;
+			break;
+		case '3':
+			i = 3;
+			break;
+		case '4':
+			i = 4;
+			break;
+		case '5':
+			i = 5;
+			break;
+		case '6':
+			i = 6;
+			break;
+		case '7':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case 'a':
+			j = 1;
+			break;
+		case 'b':
+			j = 2;
+			break;
+		case 'c':
+			j = 3;
+			break;
+		case 'd':
+			j = 4;
+			break;
+		case 'e':
+			j = 5;
+			break;
+		case 'f':
+			j = 6;
+			break;
+		case 'g':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 2; k++)
+		{
+			tabela[i][j] = 'R';
+			j++;
+		}
+	}
+
+	IzpisTabela(tabela, n, "Miha");
+
+	std::cout << "Vnesi pozicijo rusilca ([stolpec][vrstica] ali [vrstica][stolpec])" << std::endl;
+	gets_s(pozicija);
+
+	if (pozicija[0] >= 'a' && pozicija[0] <= 'g')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case 'a':
+			i = 1;
+			break;
+		case 'b':
+			i = 2;
+			break;
+		case 'c':
+			i = 3;
+			break;
+		case 'd':
+			i = 4;
+			break;
+		case 'e':
+			i = 5;
+			break;
+		case 'f':
+			i = 6;
+			break;
+		case 'g':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case '1':
+			j = 1;
+			break;
+		case '2':
+			j = 2;
+			break;
+		case '3':
+			j = 3;
+			break;
+		case '4':
+			j = 4;
+			break;
+		case '5':
+			j = 5;
+			break;
+		case '6':
+			j = 6;
+			break;
+		case '7':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 2; k++)
+		{
+			tabela[j][i] = 'R';
+			j++;
+		}
+	}
+	else if (pozicija[0] >= '1' && pozicija[0] <= '7')
+	{
+		int i, j;
+		switch (pozicija[0])
+		{
+		case '1':
+			i = 1;
+			break;
+		case '2':
+			i = 2;
+			break;
+		case '3':
+			i = 3;
+			break;
+		case '4':
+			i = 4;
+			break;
+		case '5':
+			i = 5;
+			break;
+		case '6':
+			i = 6;
+			break;
+		case '7':
+			i = 7;
+			break;
+		}
+
+		switch (pozicija[1])
+		{
+		case 'a':
+			j = 1;
+			break;
+		case 'b':
+			j = 2;
+			break;
+		case 'c':
+			j = 3;
+			break;
+		case 'd':
+			j = 4;
+			break;
+		case 'e':
+			j = 5;
+			break;
+		case 'f':
+			j = 6;
+			break;
+		case 'g':
+			j = 7;
+			break;
+		}
+
+		for (int k = 0; k < 2; k++)
+		{
+			tabela[i][j] = 'R';
+			j++;
+		}
+	}
+
+	IzpisTabela(tabela, n, "Miha");
 }
 
-void IzpisTabela(char tabela[][8], int n)
+void IzpisTabela(char tabela[][8], int n, const char* imeIgralnoPolje)
 {
+	system("cls");
+
+	std::cout << "Igralno polje @ " << imeIgralnoPolje << std::endl << std::endl;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
